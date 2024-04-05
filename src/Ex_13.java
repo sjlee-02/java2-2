494163f49ea4fea7ac93ba49c17cddf890907426
ldm2001 <01ldm@naver.com>
2024-04-05 14:58:47 +0900

    Java2 (0405)

---
+++ src/Ex_13.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex_13 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dividend;
+        int divisor;
+        
+        System.out.println("나뉨수를 입력하시오;");
+        dividend = scanner.nextInt();
+        System.out.println("나눗수를 입력하시오;");
+        divisor = scanner.nextInt();
+        try {
+            System.out.println(dividend +"를 " + divisor + "로 나누면 몫은" + dividend/divisor + "입니다.");
+        }
+        catch(ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다!");
+        }
+        finally {
+            scanner.close();
+        }
+    }
+    
+}
