e54891c687825c245fbb059e9976411137dc4921
ldm2001 <01ldm@naver.com>
2024-04-24 10:36:58 +0900

    Midterm2

--- src/Midterm.java
+++ src/Midterm.java
@@ -4,16 +4,16 @@
  * 프로그램은 무한 반복되다가 exit라는 문자열이 입력되면 종료 됩니다.
  */
 
- import ; // (문제1) 다음 코드를 실행하기 위한 Scanner class를 import해 주세요.
+ import java.util.Scanner; ; // (문제1) 다음 코드를 실행하기 위한 Scanner class를 import해 주세요.
 
  public class Midterm {
      public static void main(String[] args) {
          Scanner scanner = new Scanner(System.in);
-         while( ) { // (문제2) 무한루프를 실행할 수 있는 조건절을 입력하세요.
+         while((scanner.nextInt()) != 0) { // (문제2) 무한루프를 실행할 수 있는 조건절을 입력하세요.
              System.out.print(">>");			
              String s = scanner.nextLine(); 
-             StringBuffer sb = ; // (문제3) StringBuffer 객체 sb 생성할 수 있게 코드를 완성해 주세요.
-             if(sb.toString(). ) { // (문제4) exit가 입력됬는지 확인하는 조건절을 완성해 주세요.
+             StringBuffer sb = new StringBuffer();  // (문제3) StringBuffer 객체 sb 생성할 수 있게 코드를 완성해 주세요.
+             if(sb.toString().equals("exit")) { // (문제4) exit가 입력됬는지 확인하는 조건절을 완성해 주세요.
                  System.out.println("프로그램을 종료합니다.");	
                  break;
              }
@@ -21,7 +21,7 @@
              while(true) {
                  int i = (int)(Math.random()*26);
                  char c = (char)('a' + i); 
-                 if(sb.charAt(index) ) { // (문제5) StringBuffer 속의 문자와 삽입하는 문자가 다른지를 확인하는 조건절을 완성해 주세요.
+                 if(sb.charAt(index) !=c ) { // (문제5) StringBuffer 속의 문자와 삽입하는 문자가 다른지를 확인하는 조건절을 완성해 주세요.
                      sb.replace(index, index+1, Character.toString(c)); 
                      break; 
                  }
